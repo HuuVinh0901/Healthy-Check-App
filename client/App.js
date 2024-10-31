341b257f035bfb926baa4d1be1b5701a0f86ae7c
Vinh <phamhuuvinh0901@gmail.com>
2024-11-01 01:02:39 +0700

    Create screen layout

--- client/App.js
+++ client/App.js
@@ -1,12 +1,18 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Login from './components/Login';
+import Home from './components/Home'
+import { NavigationContainer } from '@react-navigation/native';
+const Stack= createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Login' component={Login} options={{headerShown:false}}/>
+        <Stack.Screen name='Home' component={Home} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
